632aadb908161d77fe2801b1ab4b79c68947361e
Julian <yeissonibarra@gmail.com>
2021-03-09 21:28:42 -0500

    UPDAT

--- src/layouts/Header/HeaderTop/index.tsx
+++ src/layouts/Header/HeaderTop/index.tsx
@@ -1,18 +1,16 @@
 import React, { FC } from 'react';
 
 import ThemeSwitch from '../../../components/ThemeSwitch';
+import Search from '../../../components/Search';
 
 import { ReactComponent as Logo } from '../../../assets/svg/logo.svg';
 
 import { IHeaderTop } from './interfaces';
 
 import { HeaderTopDiv } from './styles';
-import Search from '../../../components/Search';
 
 // header top
 const HeaderTop: FC<IHeaderTop> = ({ active, color, onToggle }) => {
-
-
   return (
     <HeaderTopDiv
       data-switch={active}>
